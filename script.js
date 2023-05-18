6920b56c2b5eeadf9371580a865a31e9c4680cbf
leticiacid0478 <62914555+leticiacid0478@users.noreply.github.com>
2023-05-18 14:25:37 -0700

    testing resize

--- script.js
+++ script.js
@@ -71,6 +71,41 @@ if (breadcrumbsLi.length > 1 && breadcrumbsLi[1].innerHTML == "Weeb Corner") {
   videoHeight = backgroundVideo.offsetHeight;
 }
 
+if (window.innerWidth <= 840) {
+  if (breadcrumbsLi.length == 1) {
+    contentWrapper.classList.add("small");
+  }
+  breadcrumbs.classList.add("small");
+  if (breadcrumbsLi.length > 1 && breadcrumbsLi[1].innerHTML == "Weeb Corner") {
+    drStoneWrapper.classList.add("small");
+    cellsAtWorkWrapper.classList.add("small");
+  }
+} else {
+  if (breadcrumbsLi.length == 1) {
+    contentWrapper.classList.remove("small");
+  }
+  breadcrumbs.classList.remove("small");
+  if (breadcrumbsLi.length > 1 && breadcrumbsLi[1].innerHTML == "Weeb Corner") {
+    drStoneWrapper.classList.remove("small");
+    cellsAtWorkWrapper.classList.remove("small");
+  }
+}
+
+if (window.innerWidth < 1400) {
+  navLinkWrappers.forEach((element) => {
+    element.classList.add('small');
+  });
+  rightNav.classList.add("small");
+  arrowWrapper.style.visibility = "visible";
+} else {
+  navLinkWrappers.forEach((element) => {
+    element.classList.remove('small');
+  });
+  rightNav.classList.remove("small");
+  arrowWrapper.style.visibility = "hidden";
+  navSmall.classList.remove("active");
+}
+
 window.onresize = function() {
 
   if (breadcrumbsLi.length > 1 && breadcrumbsLi[1].innerHTML == "Weeb Corner") {
